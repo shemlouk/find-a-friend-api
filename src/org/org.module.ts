c5458ba3e5810fa46f81ebf5898ccc92e3261232
samuel <shemlouk@gmail.com>
2024-01-01 23:10:13 -0300

    feat: add prisma service to nest app

--- src/org/org.module.ts
+++ src/org/org.module.ts
@@ -1,9 +1,11 @@
+import { PrismaService } from '@/prisma.service';
 import { CepApi } from '@core/application/contracts/cep-api';
 import { RegisterOrg } from '@core/application/use-cases/register-org';
 import { OrgRepository } from '@core/domain/repositories/org-repository';
 import { ViaCepApiAdapter } from '@core/infra/adapters/via-cep-api-adapter';
 import { RegisterOrgPresenter } from '@core/infra/presenters/register-org-presenter';
 import { OrgInMemoryRepository } from '@core/infra/repositories/in-memory/org-in-memory-repository';
+import { OrgPrismaRepository } from '@core/infra/repositories/prisma/org-prisma-repository';
 import { Module } from '@nestjs/common';
 import { OrgController } from './org.controller';
 import { OrgService } from './org.service';
@@ -13,14 +15,20 @@ import { OrgService } from './org.service';
   providers: [
     OrgService,
     { provide: OrgInMemoryRepository, useClass: OrgInMemoryRepository },
-    { provide: ViaCepApiAdapter, useClass: ViaCepApiAdapter },
     { provide: RegisterOrgPresenter, useClass: RegisterOrgPresenter },
+    { provide: ViaCepApiAdapter, useClass: ViaCepApiAdapter },
+    { provide: PrismaService, useClass: PrismaService },
     {
       provide: RegisterOrg,
-      useFactory: (repo: OrgRepository, api: CepApi) => {
-        return new RegisterOrg(repo, api);
+      useFactory: (orgRepository: OrgRepository, cepApi: CepApi) => {
+        return new RegisterOrg(orgRepository, cepApi);
       },
-      inject: [OrgInMemoryRepository, ViaCepApiAdapter],
+      inject: [OrgPrismaRepository, ViaCepApiAdapter],
+    },
+    {
+      provide: OrgPrismaRepository,
+      useFactory: (prisma: PrismaService) => new OrgPrismaRepository(prisma),
+      inject: [PrismaService],
     },
   ],
 })
